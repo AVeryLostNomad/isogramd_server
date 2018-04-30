95a8b3cf3ab6347f22da0499db18875cde84f4d6
Joey Maher <Joey.E.Maher-1@ou.edu>
2018-04-30 12:57:40 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name='isogram-server',
     author='me',
     author_email='me@gmail.com',
     url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Flask','flask-wtf','flask-babel','markdown','flup', 'numpy'],
+    install_requires=['Flask==0.12.2','numpy==1.14.0'],
 )
